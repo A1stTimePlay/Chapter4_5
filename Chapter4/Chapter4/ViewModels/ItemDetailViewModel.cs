1c074b43549492ff4ef8a3e87aeb3cf8427b8bce
NguyenXuanAn <nguyenxuanan.nxa@gmail.com>
2019-10-31 23:42:21 +0700

    Add project files.

---
+++ Chapter4/Chapter4/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Chapter4.Models;
+
+namespace Chapter4.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
