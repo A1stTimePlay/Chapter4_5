945ad292e9ea59997fc2835f6ea51005e5c04d81
NguyenXuanAn <nguyenxuanan.nxa@gmail.com>
2019-11-01 14:06:48 +0700

    complete chapter 6

---
+++ Chapter6/Chapter6/ListViewContentAction.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Chapter6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewContentAction : ContentPage
+    {
+        public ListViewContentAction()
+        {
+            InitializeComponent();
+        }
+
+        public async void MoreClicked(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            var item = (ListItem)(mi.CommandParameter);
+            await DisplayAlert("Clicked", item.Title.ToString() + " More button was clicked", "OK");
+}
+        public async void DeleteClicked(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            var item = (ListItem)mi.CommandParameter;
+            await DisplayAlert("Clicked", item.Title.ToString() + " Delete button was clicked", "OK");
+        }
+
+        public class ListItem
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+        }
+    }
+}
